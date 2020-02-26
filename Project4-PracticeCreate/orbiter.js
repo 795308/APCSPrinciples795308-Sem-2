1568943c8dadc83e5ea6a6448b0a67d804caa93e
795308 <54591256+795308@users.noreply.github.com>
2020-02-26 08:49:51 -0800

    Class 2/26/20

--- Project4-PracticeCreate/orbiter.js
+++ Project4-PracticeCreate/orbiter.js
@@ -1,11 +1,25 @@
 class Orbiter{
-  constructor(){
+  constructor(id){
     this.loc = createVector(0, 0);//location vector
     this.vel = createVector(0, 0);//velocity vector
     this.acc = createVector(0, 0);//acceleration vector
     this.clr = color(random(0, 255), random(0, 255), random(0, 255));
+    this.id = id
+    for(var i = 0; i < orbiters.length; i++){
+      this.time = TWO_PI*(i/orbiters.length);
+    }
   }
   run(){
-    
+    this.update();
+    this.render();
+  }
+  update(){
+    this.loc.x = mainBall.loc.x+100*cos(this.time);
+    this.loc.y = mainBall.loc.y+100*sin(this.time);
+    this.time = this.time + 0.05;
+  }
+  render(){
+    fill(this.clr);
+    ellipse(this.loc.x, this.loc.y, 10, 10);
   }
 }
--- Project4-PracticeCreate/sketch.js
+++ Project4-PracticeCreate/sketch.js
@@ -3,13 +3,14 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var mainBall;
-var boids = []//array for boids
+var boids = [];//array for boids
+var orbiters = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  loadBoids(50);
+  loadBoids(50, 2);
 
 
 }
@@ -19,15 +20,21 @@ function draw() {
   //background(5, 5, 5, 10);
   runBoids();
 }
-function loadBoids(n){
+function loadBoids(n, o){
   mainBall = new Ball(400, 400, random(2.5, -2.5), random(2.5, -2.5));
   for(var i = 0; i < n; i++){
     boids[i] = new Boid(random(width), random(height), random(-1, 1), random(-1, 1), i);
+  }
+  for(var i = 0; i < o; i++){
+    orbiters[i] = new Orbiter(i);
   }
 }//loads boids in array
 function runBoids(){
   mainBall.run();
   for(var i = 0; i < boids.length; i++){
     boids[i].run();
+  }
+  for(var i = 0; i < orbiters.length; i++){
+    orbiters[i].run
   }
 }//runs all boids in array
