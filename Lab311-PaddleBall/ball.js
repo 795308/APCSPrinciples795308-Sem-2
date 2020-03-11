1f54bf26ed55370ab6bcd1b5cc076db60b153e9d
795308 <54591256+795308@users.noreply.github.com>
2020-03-11 08:50:21 -0700

    Class 3/11/20

---
+++ Lab311-PaddleBall/ball.js
@@ -0,0 +1,81 @@
+//  First Constructor Function
+// eettlin
+// March 10, 2020
+
+class Ball{
+ //  Ball Properties  +++++++++++++++++++++++++++++++++++++++++++++
+ constructor(x, y, dx, dy, id){
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.acc = createVector(0,0.016);
+    this.w = 15;
+    this.id = id;
+    if(this.id < 0) {this.w = 50;}
+ }
+
+ run(){
+   this.checkEdges();
+   this.update();
+   this.render();
+   this.isColliding();
+   this.remove();
+ }
+
+
+  checkEdges(){//+++++++++++++++++++++++++++++++++++++++++++++++++++
+    if(this.loc.x < 0){
+      this.vel.x = -this.vel.x;
+    }
+    if(this.loc.x > width){
+      this.vel.x = -this.vel.x;
+    }
+    if(this.loc.y < 0){
+      this.vel.y = -this.vel.y;
+    }
+    if(this.loc.y > height){
+      this.vel.y = -this.vel.y;
+      this.loc.y = height-2;
+    }
+  } //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  update(){//++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    var distToMainBall;
+    if(this.id >= 0){//  if not mainBall
+
+
+    }
+
+    this.vel.add(this.acc);
+    this.vel.limit(5);
+    this.loc.add(this.vel);
+  }//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+ render(){
+  //  **************************************************************
+   if(this.id < 0){
+    fill(255,0,0);
+  }else if(this.id%2 === 0){
+    fill(0,255,0);
+   }else{
+    fill(0,0,255);
+   }
+   ellipse(this.loc.x, this.loc.y, this.w, this.w);
+ }//***************************************************************
+
+ isColliding(){
+    if(this.loc.x > paddle.loc.x - paddle.w/2 && this.loc.x < paddle.loc.x + paddle.w/2 && this.loc.y > paddle.loc.y - paddle.h/2 && this.loc.y < paddle.loc.y + paddle.h/2){
+        return true
+       }
+  }//checks if the ball is colliding with the paddle
+
+  remove(){
+    for(var i = balls.length-1; i>=0; i--){
+      if(balls[i].isColliding()){
+        if(balls[i].id === -1){
+          gameState = 1
+        }
+        balls.splice(i, 1);
+      }
+    }
+  }//removes balls that touch the paddle from the array.
+}//  end Ball class
---
+++ Lab311-PaddleBall/sketch.js
@@ -0,0 +1,45 @@
+
+
+//  eettlin
+//  306
+var paddle, mainBall;
+var balls  = [];
+var gameState = 0
+// Setup runs once at the start of your program
+
+
+function setup() {
+  // put start code here
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20,20,20);//  back ground color
+  paddle = new Paddle(width/2, height - 100, 100, 10);
+  mainBall = new Ball(width/2, 20, random(-3, 3), random(-3, 3), -1);
+  loadBalls(33);
+}
+
+//  Draw runs 30 times each second
+function draw() {
+  // put drawing code here
+    background(20,20,20, 20);//  back ground color
+    if(gameState === 0){
+      paddle.run();
+      mainBall.run();
+      for(var i =0; i < balls.length; i++){
+        balls[i].run();
+      }
+    }else if (gameState === 1) {
+      balls.splice(0, balls.length);//removes all balls from the array, so there are none carried over into the next game
+      textAlign(CENTER);
+      textSize(100);
+      fill(255, 0, 0);
+      text("YOU LOSE", 400, 400);//writes a red YOU LOSE on the screen
+    }
+}
+
+function loadBalls(numBalls){
+  for(var i =0; i < numBalls; i++){
+    balls[i] = new Ball(random(width),50, random(-3.0, 3.0), random(-3.0, 3.0), i);
+  }
+
+}
